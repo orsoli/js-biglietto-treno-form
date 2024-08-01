efa31b74dd4f58e70e4177a366fd26f57d38bea5
orsoli <fljcoli@gmail.com>
2024-08-01 14:52:57 +0200

    added preparation phase

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-console.log("js ok");
+/*
+    - Receive the interested of element on DOM
+    - Create variable for the price per 1 km
+    - Create the discounts variable of value null
+    - Recieve the user values from inputs forms
+    - Cacolate final Price
+    M1
+    - Print the final price in consol
+    M2
+    - Print the final price in page
+    Bonus
+    - 
+*/
+
+//--- Preparation phase
+//Receive the interested element from DOM
+const button = document.getElementById("pay-btn");
+const distanceField = document.getElementById("distance");
+const ageField = document.getElementById("age");
+// Price per kilometer
+const kmPrice = 0.21;
+console.log(`Price per kilometer ${kmPrice} $/km`);
+// Prices messages
+const messagePrice = "Your ticket price is:";
+// Possible discount
+let discount = null;
