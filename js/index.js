10fac9b0c0aeb33deca8a2b38197be1d818f5d2c
orsoli <fljcoli@gmail.com>
2024-08-01 22:19:07 +0200

    added validation

--- js/index.js
+++ js/index.js
@@ -28,7 +28,9 @@ const nameField = document.getElementById("name");
 const resultpassengerName = document.getElementById("passenger-name");
 const resultCarraige = document.getElementById("carraige");
 const resultCpCode = document.getElementById("cp-code");
-
+//--Valdation
+const errorMssg = `<strong class = "text-danger-emphasis">!ERROR:</strong> Please insert in the correct way requared details`;
+const resultError = document.getElementById("ticket-container");
 // Price per kilometer
 const kmPrice = 0.21;
 console.log(`Price per kilometer ${kmPrice} €/km`);
@@ -44,74 +46,72 @@ button.addEventListener("click", function () {
   //Receive the inputs values
   const distance = distanceField.value;
   const age = ageField.value;
-  const name = nameField.value;
+  const name = nameField.value.trim();
 
   //! Validation
-  //   if (age <= 0 || discount <= 0) {
-  //     alert("Insert numbers > 0");
-  //     form.reset();
-  //   }
-
-  // Calcolate the final price
-  let ticketPrice = (kmPrice * distance).toFixed(2);
-  let standartPrice = ticketPrice;
-  // Calcolate the discounts
-  if (age == "underage") discount = 20;
-  else if (age == "senior") discount = 40;
-
-  if (discount) {
-    //Discount mssg
-    const discountMssg = `You have received a ${discount}% discount.`;
-    //Print discount mssg in page
-    resultInfo.innerHTML = `<strong>${discountMssg}</strong>`;
-    //Print discount mssg in consol
-    console.log(discountMssg);
-
-    //Calcolate the discount amount
-    const discountAmount = ((ticketPrice / 100) * discount).toFixed(2);
-    //Print in consol
-    console.log(`Your discount amount is ${discountAmount} €`);
-
-    //Calcolate the ticket price in discount conditions
-    ticketPrice -= discountAmount;
-    //Print ticket price in page
-    resultPrice.innerHTML = `${messagePrice} ${ticketPrice}€  <del class='text-danger-emphasis fst-italic'>${standartPrice}€</del>`;
-    //Print ticket price in consol
-    console.log(`${messagePrice} ${ticketPrice} €`);
+  if (!name || !isNaN(name) || distance <= 0) {
+    resultError.innerHTML = errorMssg;
   } else {
-    // Print the ticket price in page
-    resultPrice.innerHTML = `${messagePrice} <strong>${ticketPrice}$</strong>`;
-    // Print the ticket price in consol
-    console.log(`${messagePrice} ${ticketPrice} €`);
-    //Print discount mssg in page
-    resultInfo.innerHTML = `Standart Ticket`;
-  }
-  //Print info in Pge
-  resultInfo.innerText += `
+    // Calcolate the final price
+    let ticketPrice = (kmPrice * distance).toFixed(2);
+    let standartPrice = ticketPrice;
+    // Calcolate the discounts
+    if (age == "underage") discount = 20;
+    else if (age == "senior") discount = 40;
+
+    if (discount) {
+      //Discount mssg
+      const discountMssg = `You have received a ${discount}% discount.`;
+      //Print discount mssg in page
+      resultInfo.innerHTML = `<strong>${discountMssg}</strong>`;
+      //Print discount mssg in consol
+      console.log(discountMssg);
+
+      //Calcolate the discount amount
+      const discountAmount = ((ticketPrice / 100) * discount).toFixed(2);
+      //Print in consol
+      console.log(`Your discount amount is ${discountAmount} €`);
+
+      //Calcolate the ticket price in discount conditions
+      ticketPrice -= discountAmount;
+      //Print ticket price in page
+      resultPrice.innerHTML = `${messagePrice} ${ticketPrice}€  <del class='text-danger-emphasis fst-italic'>${standartPrice}€</del>`;
+      //Print ticket price in consol
+      console.log(`${messagePrice} ${ticketPrice} €`);
+    } else {
+      // Print the ticket price in page
+      resultPrice.innerHTML = `${messagePrice} <strong>${ticketPrice}$</strong>`;
+      // Print the ticket price in consol
+      console.log(`${messagePrice} ${ticketPrice} €`);
+      //Print discount mssg in page
+      resultInfo.innerHTML = `Standart Ticket`;
+    }
+    //Print info in Pge
+    resultInfo.innerText += `
         Price per kilometer ${kmPrice}€/km.
         Your destination is ${distance} KM from here.
         `;
 
-  //--Bonus
-  //Create max rendom numbers
-  const maxCarraige = 20;
-  const maxCpcode = 100000;
-  //--Generating numbers
-  // Carraige number
-  const carraigeNumber = Math.floor(Math.random() * maxCarraige) + 1;
-  console.log(carraigeNumber);
-  // Cp code number
-  const cpCode = Math.floor(Math.random() * maxCpcode) + 1;
-  console.log(cpCode);
+    //--Bonus
+    //Create max rendom numbers
+    const maxCarraige = 20;
+    const maxCpcode = 100000;
+    //--Generating numbers
+    // Carraige number
+    const carraigeNumber = Math.floor(Math.random() * maxCarraige) + 1;
+    console.log(carraigeNumber);
+    // Cp code number
+    const cpCode = Math.floor(Math.random() * maxCpcode) + 1;
+    console.log(cpCode);
 
-  //--Processing
-  // Print the passenger name on page
-  resultpassengerName.innerHTML += name;
-  // Print carraige nr  on page
-  resultCarraige.innerHTML += `<em>${carraigeNumber}</em>`;
-  // Print Cp code on page
-  resultCpCode.innerHTML += `<em>${cpCode}</em>`;
-
+    //--Processing
+    // Print the passenger name on page
+    resultpassengerName.innerHTML += name;
+    // Print carraige nr  on page
+    resultCarraige.innerHTML += `<em>${carraigeNumber}</em>`;
+    // Print Cp code on page
+    resultCpCode.innerHTML += `<em>${cpCode}</em>`;
+  }
   //Reset form
   form.reset();
 });
